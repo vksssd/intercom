2cbc3ced7138b2b4df50ff7ff3057e4696edd92e
vksssdks <2820142.cse@pietgroup.co.in>
2024-06-07 17:23:06 +0530

    initial

--- auth-service/app/main.go
+++ auth-service/app/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World checking merge conflict!")
 }
 
 func main(){
-	
+
 	http.HandleFunc("/", handler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil)) // http.ListenAndServe(":8080", nil)
